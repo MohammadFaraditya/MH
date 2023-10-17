36271590590daa9a977f3ec13ae22af2ce6bdc34
MohammadFaraditya <mohammadfaraditya@gmail.com>
2023-10-17 20:54:01 +0700

    pindahan ke github

---
+++ login/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/login.html"],
+  theme: {
+    extend: {
+      colors: {
+        'bgmh': '#222222',
+        'gold': '#c9943e',
+      }
+    },
+  },
+  plugins: [],
+}
+
